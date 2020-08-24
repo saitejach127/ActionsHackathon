311663e2178d230802248e4a883b79d1e6f6e3b6
saitejach127 <saitejach127@gmail.com>
2020-08-24 21:03:59 +0530

    To test the action

--- actions/dist/index.js
+++ actions/dist/index.js
@@ -6191,16 +6191,7 @@ const axios = __webpack_require__(366);
 
 try {
     const payload = github.context.payload;
-    console.log(payload);
-    payload.commits.forEach((commit) => {
-        var username = commit.commiter.username;
-        axios.default.post('https://reward-keeper.herokuapp.com/rewards/',{
-            username : username,
-            points : 10
-        }).then((res) => {
-            console.log(`${username} rewarded with 10 points`);
-        })
-    })
+    console.log(payload.commits);
 } catch (e) {
     core.setFailed(e.message);
 }
--- actions/index.js
+++ actions/index.js
@@ -4,16 +4,7 @@ const axios = require('axios');
 
 try {
     const payload = github.context.payload;
-    console.log(payload);
-    payload.commits.forEach((commit) => {
-        var username = commit.commiter.username;
-        axios.default.post('https://reward-keeper.herokuapp.com/rewards/',{
-            username : username,
-            points : 10
-        }).then((res) => {
-            console.log(`${username} rewarded with 10 points`);
-        })
-    })
+    console.log(payload.commits);
 } catch (e) {
     core.setFailed(e.message);
 }
