2b8f5bdf13cc085412f11b02b4555868bdf0b80a
saitejach127 <saitejach127@gmail.com>
2020-09-13 14:45:50 +0530

    To test the action

--- actions/dist/index.js
+++ actions/dist/index.js
@@ -6192,13 +6192,15 @@ const axios = __webpack_require__(366);
 try {
     const payload = github.context.payload;
     console.log(payload.repository.full_name);
-    
+    var repoName = payload.repository.full_name.replace("/", "_");
+    console.log(repoName);
     payload.commits.forEach((commit) => {
         var username = commit.committer.username;
         var points = 10;
         axios.default.post('https://reward-keeper.herokuapp.com/rewards/', {
             "username" : username,
-            "points" : points
+            "points" : points,
+            "repoName": repoName
         }).then((res) => {
             console.log(`${username} rewarded with ${points} points`);
         })
