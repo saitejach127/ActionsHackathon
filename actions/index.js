c532e3c282980157f5ff3ecebae9126121f3dda5
saitejach127 <saitejach127@gmail.com>
2020-09-13 14:37:39 +0530

    Repo level rewarding added

--- actions/index.js
+++ actions/index.js
@@ -5,13 +5,15 @@ const axios = require('axios');
 try {
     const payload = github.context.payload;
     console.log(payload.repository.full_name);
-    
+    var repoName = payload.repository.full_name.replace("/", "_");
+    console.log(repoName);
     payload.commits.forEach((commit) => {
         var username = commit.committer.username;
         var points = 10;
         axios.default.post('https://reward-keeper.herokuapp.com/rewards/', {
             "username" : username,
-            "points" : points
+            "points" : points,
+            "repoName": repoName
         }).then((res) => {
             console.log(`${username} rewarded with ${points} points`);
         })
--- index.js
+++ index.js
@@ -16,33 +16,65 @@ admin.initializeApp({
 
 var db = admin.database();
 
-app.get("/", (req,res) => {
-    res.sendFile("public/index.html");
-})
+app.get("/", (req, res) => {
+  res.sendFile("public/index.html");
+});
 
-app.get("/rewards/:username", async (req, res) => {
+app.get("/user/:username", async (req, res) => {
   var name = req.params.username;
-  var data = await db.ref("rewards/" + name).once("value");
+  var data = await db.ref("rewards/").once("value");
   if (data.val() === null) {
-    res.json({ name: name, points: 0 });
+    return res.json({ name: name, points: 0 });
   } else {
-    var points = data.val().points;
-    console.log(data.val());
-    res.json({ name: name, points: points });
+    data = data.val();
+    var repos = []
+    for(const key of Object.keys(data)){
+      if(data[key][name]){
+        repos.push({
+          repo: key,
+          points: data[key][name]["points"]
+        })
+      }
+    }
+    return res.json({ name: name, contributions: repos });
+  }
+});
+
+app.get("/repo/:repoName", async (req, res) => {
+  var repoName = req.params.repoName;
+  var data = await db.ref("rewards/" + repoName).once("value");
+  var contributors = [];
+  data = data.val();
+  console.log(data);
+  for (const key of Object.keys(data)){
+    var temp = {};
+    temp[key] = data[key]["points"]
+    contributors.push(temp);
+  }
+  console.log(contributors);
+  if (data === null) {
+    return res.json({ repoName: repoName, result: "No contributors yet" });
+  } else {
+    return res.json({
+      repoName,
+      contributors
+    });
   }
 });
 
 app.post("/rewards", async (req, res) => {
   var name = req.body.username;
   var newPoints = req.body.points;
-  var data = await db.ref("rewards/" + name).once("value");
+  var repoName = req.body.repoName;
+  var data = await db.ref(`rewards/${repoName}/${name}`).once("value");
+  console.log(`rewards/${repoName}/${name}`);
   let points;
   if (data.val() === null) {
     points = 0;
   } else {
     points = data.val().points;
   }
-  db.ref("rewards/" + name).set({
+  db.ref(`rewards/${repoName}/${name}`).set({
     points: parseInt(points) + parseInt(newPoints),
   });
   res.json({ success: true });
